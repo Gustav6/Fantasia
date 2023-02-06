cdcb63d0c203159097266f51611b77c07658e4ce
ANDOBRUH <122355264+ANDOBRUH@users.noreply.github.com>
2023-02-06 11:01:43 +0100

    Dialogue System
    NPC prefab redo att slänga ut, allt man måste göra är att ändra spriten så att bilden blir rätt och lägga till hur många som ska vara med i konversationen och vad de ska säga.

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+            trigger.startDialogue();
+    }
+}

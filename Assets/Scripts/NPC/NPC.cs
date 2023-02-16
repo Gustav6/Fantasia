7bffb61d4c8b477ae4ae17e62df864c9903f7770
ANDOBRUH <122355264+ANDOBRUH@users.noreply.github.com>
2023-02-16 13:29:06 +0100

    Added new level

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -7,19 +7,24 @@ public class NPC : MonoBehaviour
 {
     public DialogueTrigger trigger;
     public DialogueManager begin;
+    public ChickenTalking triggerChicken;
 
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") == true)
+        {
             trigger.startDialogue();
-        //Startar dialoguen när man går in i NPCs hitbox
+        
+        }   
+        
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") == true)
         {
                 begin.ExitDialogueIfToFarAway();
+              
         }
     }
 }

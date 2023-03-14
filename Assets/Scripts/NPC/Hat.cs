805cc10e0cce8b8b78da5ea3f71ec1f18e45c090
SkieoXDD <119525838+SkieoXDD@users.noreply.github.com>
2023-03-14 11:01:41 +0100

    Fixed whale

--- Assets/Scripts/NPC/Hat.cs
+++ Assets/Scripts/NPC/Hat.cs
@@ -8,9 +8,12 @@ public class Hat : MonoBehaviour
 {
     public GameObject hat;
     public bool hatInInventory = false;
+    public GameObject whaleWithoutHat;
+    public GameObject whaleWithHat;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         hat.SetActive(false);
-        hatInInventory = true;   
+        hatInInventory = true;
+        whaleWithoutHat.SetActive(false); whaleWithHat.SetActive(true);
     }
 }
